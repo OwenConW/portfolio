b6131e299193caea2f58fc6d2c2d8e9cd5e6e719
OwenConW <Patricios78owen@gmail.com>
2022-09-13 21:56:59 -0300

    Home responsive finished

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -2,20 +2,23 @@ import React from "react"
 import s from "./home.module.scss"
 import hacker from "./assets/hacker.png"
 import circulo from "./assets/Ellipse1.png"
+import portfolioIcon from "./assets/portfolio.png"
 import Nav from "../Nav/Nav"
+import { motion } from "framer-motion"
 
 
 const Home = () => {
+
     return (
         <div className={s.contenedorPadre}>
             <div className={s.contenedorHeader}>
                 <div className={s.contenedorTitle}>
-                <b>{`>`}</b><h1 className={s.title}>Bienvenid<b className={s.o}>x</b> al portfolio de <b className={s.o}>O</b>wen...</h1>    
-                <div className={s.MyName}>
+                <motion.b initial={{x:-4000, opacity: 0}} animate={{ x: 0, opacity: 1 }} transition={{delay: 0.5}}>{`>`}</motion.b><motion.h1 initial={{x:-4000, opacity: 0}} animate={{ x: 0, opacity: 1 }} transition={{delay: 0.5}} className={s.title}>Bienvenid<b className={s.o}>@</b> al portfolio de <b className={s.o}>O</b>wen.....</motion.h1>    
+                <motion.div className={s.MyName} animate={{ x: [700, 0], y: [0, 0],  opacity: [0, 1] }}>
                     <div>
-                        <h1 className={s.name}>OWEN NICOLAS PEREZ BONORIS</h1>
+                        <img src={portfolioIcon} alt="icon" className={s.portfolioIcon}/>
                     </div>
-                </div>
+                </motion.div>
                 </div>
             </div>
             <div className={s.contenedorImageNav}>
@@ -25,14 +28,14 @@ const Home = () => {
                     <b className={s.b3}>me motiva aprender nuevas</b>
                     <b className={s.b4}>tecnologias, así como a su vez </b>
                     <b className={s.b5}> adentrarme en el mundo de la </b>
-                    <b className={s.b6}><b className={s.inicio}>ciberseguridad</b> y seguir </b>
+                    <b className={s.b6}><b className={s.inicio}>ciberseguridad</b> y continuar </b>
                     <b className={s.b7}> aprendiendo Ingles.</b>
                 </div>
-            <div className={s.visual}>
+            <motion.div className={s.visual} animate={{ y: [700, 0], opacity: [0, 1] }} >
                 <img src={circulo} alt="sombra" className={s.Circulo}/>
                 <img src={circulo} alt="sombra" className={s.Circulo2}/>
                 <img src={hacker} alt="sombra" className={s.hacker}/>
-            </div>
+            </motion.div>
             <div className={s.contenedorLista}>
                 <Nav></Nav>
             </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,85 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import s from "./Nav.module.scss"
-
+import { motion } from "framer-motion"
+import { useSelector, useDispatch } from "react-redux"
+import { changeSelectedWindow} from "../../redux/actions/actions"
 
 const Nav = () =>  {
+
+    const dispatch = useDispatch()
+
+    const selected = useSelector(state => state.selected)
+
+    const handleClick = (e) => {
+        dispatch(changeSelectedWindow(e.target.id))
+        console.log(e.target)
+    } 
+
     return(
         <>
-        <ul className={s.lista}>
-                    <li className={s.item}><Link to="/about" className={s.Link}>.SOBRE_MI<b className={s.parentesis}>()</b></Link></li>
-                    <li className={s.item}><Link to="/proyects" className={s.Link}>.PROYECTOS<b className={s.parentesis}>()</b></Link></li>
-                    <li className={s.item}><Link to="/contact" className={s.Link}>.CONTACTAME<b className={s.parentesis}>()</b></Link></li>
-                    <li className={s.item}><Link to="/tech" className={s.Link}>.TECNOLOGIAS<b className={s.parentesis}>()</b></Link></li>
-                </ul>
+            <ul className={s.lista}>
+                {
+                    console.log("TAMAÑO:", window.screen.width)
+                }
+                {
+                    window.visualViewport.width <= 688 ? (
+                        <>
+                        <motion.li className={selected === "home" ? s.selected : s.item} 
+                        inital={{x: 100}} 
+                        animate={{ y: [700, 0], x: [0, 0] }} 
+                        transition={{delay: 0.5}}  
+                        onClick={handleClick}><Link id={"home"} to="/" className={s.Link}>.SOBRE_MI<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        <motion.li className={selected === "projects" ? s.selected : s.item} 
+                        inital={{x: 100}}  
+                        animate={{ y: [700, 0], x: [0, 0] }} 
+                        transition={{delay: 1}}  
+                        onClick={handleClick}><Link id={"projects"} to="/proyects" className={s.Link}>.PROYECTOS<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        <motion.li className={selected === "contactme" ? s.selected : s.item} 
+                        inital={{x: 100}}  
+                        animate={{ y: [700, 0], x: [0, 0] }} 
+                        transition={{delay: 1.5}}  
+                        onClick={handleClick}><Link id={"contactme"} to="/contact" className={s.Link}>.CONTACTAME<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        <motion.li className={selected === "tecnologies" ? s.selected : s.item} 
+                        inital={{x: 100}}  
+                        animate={{ y: [700, 0], x: [0, 0] }} 
+                        transition={{delay: 2}}  
+                        onClick={handleClick}><Link id={"tecnologies"} to="/tech" className={s.Link}>.TECNOLOGIAS<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        </>
+                    ) : (
+                        <>
+                        <motion.li className={selected === "home" ? s.selected : s.item} 
+                        inital={{x: 100}} 
+                        animate={{ y: [-700, 0], x: [0, 0] }} 
+                        transition={{delay: 0.5}}  
+                        onClick={handleClick}><Link id={"home"} to="/" className={s.Link}>.SOBRE_MI<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        <motion.li className={selected === "projects" ? s.selected : s.item} 
+                        inital={{x: 100}}  
+                        animate={{ y: [-700, 0], x: [0, 0] }} 
+                        transition={{delay: 1}}  
+                        onClick={handleClick}><Link id={"projects"} to="/proyects" className={s.Link}>.PROYECTOS<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        <motion.li className={selected === "contactme" ? s.selected : s.item} 
+                        inital={{x: 100}}  
+                        animate={{ y: [-700, 0], x: [0, 0] }} 
+                        transition={{delay: 1.5}}  
+                        onClick={handleClick}><Link id={"contactme"} to="/contact" className={s.Link}>.CONTACTAME<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        <motion.li className={selected === "tecnologies" ? s.selected : s.item} 
+                        inital={{x: 100}}  
+                        animate={{ y: [-700, 0], x: [0, 0] }} 
+                        transition={{delay: 2}}  
+                        onClick={handleClick}><Link id={"tecnologies"} to="/tech" className={s.Link}>.TECNOLOGIAS<b className={s.parentesis}>()</b></Link>
+                        </motion.li>
+                        </>
+                    )
+                }
+            </ul>
         </>
     )
 }
